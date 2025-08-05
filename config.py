b7e8fda383f8cd2fdd71c7c2185090790e4c7dc0
ErickJ3ai <erickjordan064@gmail.com>
2025-08-05 13:43:47 -0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,4 @@
+# Reemplaza con los IDs correspondientes de tu servidor
+GUILD_ID = 1398140270801060062  # ID de tu Servidor de Discord
+ADMIN_ROLE_NAME = "AdminBot"    # Nombre del rol que podrá usar comandos de admin
+REDEMPTION_LOG_CHANNEL_ID = 1402046653950722118 # Canal para alertas de canjeo
